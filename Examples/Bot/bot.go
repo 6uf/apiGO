d56a0e3a3ef936b99b2a26cc92fdba4ee8c92165
Liza-Developer <86650306+Liza-Developer@users.noreply.github.com>
2022-01-08 17:31:28 -0400

    Add files via upload

---
+++ Examples/Bot/bot.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Liza-Developer/apiGO"
+)
+
+func main() {
+	apiGO.Bot()
+}
---
+++ Examples/Sniper/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/Liza-Developer/apiGO"
+	"github.com/logrusorgru/aurora/v3"
+)
+
+var (
+	name  string
+	delay float64
+)
+
+func main() {
+
+	fmt.Print(" Name: \n>>")
+	fmt.Scan(&name)
+	fmt.Print("Delay: \n>>")
+	fmt.Scan(&delay)
+
+	fmt.Println()
+
+	bearers, _ := apiGO.Auth()
+	dropTime := apiGO.DropTime(name)
+
+	apiGO.PreSleep(dropTime)
+
+	paload := bearers.CreatePayloads(name)
+
+	apiGO.Sleep(dropTime, delay)
+
+	var wg sync.WaitGroup
+
+	fmt.Println()
+
+	for e, acc := range paload.AccountType {
+		if acc == "Giftcard" {
+			for i := 0; i < 6; i++ {
+				wg.Add(1)
+				go func() {
+					send, recv, status := paload.SocketSending(int64(e))
+					if status == "200" {
+						fmt.Printf("Sent @ %v ~ [%v] @ %v \n", send.Format("05.00000"), aurora.Green("SUCCESS"), recv.Format("05.00000"))
+					} else {
+						fmt.Printf("Sent @ %v ~ [%v] @ %v \n", send.Format("05.00000"), aurora.Red(status), recv.Format("05.00000"))
+					}
+					wg.Done()
+				}()
+			}
+		} else {
+			for i := 0; i < 2; i++ {
+				wg.Add(1)
+				go func() {
+					send, recv, status := paload.SocketSending(int64(e))
+					if status == "200" {
+						fmt.Printf("Sent @ %v ~ [%v] @ %v \n", send.Format("05.00000"), aurora.Green("SUCCESS"), recv.Format("05.00000"))
+					} else {
+						fmt.Printf("Sent @ %v ~ [%v] @ %v \n", send.Format("05.00000"), aurora.Red(status), recv.Format("05.00000"))
+					}
+					wg.Done()
+				}()
+			}
+		}
+	}
+
+	wg.Wait()
+}
