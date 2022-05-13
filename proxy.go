d61c6cd6ba9d3bd08424f55dc307bceee2555785
Brandon R <brandon.p.r@hotmail.com>
2022-05-13 14:58:07 -0200

    Fixed proxy sniping

--- proxy.go
+++ proxy.go
@@ -36,7 +36,6 @@ func (Proxy *Proxys) Setup() {
 func (Proxy *Proxys) RandProxy() string {
 	for {
 		rand.Seed(time.Now().UnixNano())
-		fmt.Println(Proxy.Proxys)
 		proxy := (*Proxy.Proxys)[rand.Intn(len(*Proxy.Proxys))]
 		if !Proxy.Used[proxy] {
 			Proxy.Used[proxy] = true
